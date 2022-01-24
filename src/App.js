d9e6ca4caf6728018bf7fec14ad688e424d306da
Celdama <celdama.dev@gmail.com>
2022-01-24 09:41:03 +0100

    App: intro to useEffect hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [color, setColor] = useState('black');
+
+  useEffect(() => {
+    const changeColorOnClick = () => {
+      if (color === 'black') {
+        setColor('red');
+      } else {
+        setColor('black');
+      }
+    };
+
+    document.addEventListener('click', changeColorOnClick);
+
+    return () => {
+      document.removeEventListener('click', changeColorOnClick);
+    };
+  }, [color]);
+
   return (
     <div className='App'>
       <h1>Hooks intro</h1>
+      <div
+        id='myDiv'
+        style={{
+          color: 'white',
+          width: '100px',
+          height: '100px',
+          position: 'absolute',
+          left: '50%',
+          top: '50%',
+          backgroundColor: color,
+        }}
+      >
+        This div can change color. Click on me
+      </div>
     </div>
   );
-}
+};
 
 export default App;
